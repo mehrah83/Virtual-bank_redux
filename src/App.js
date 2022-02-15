429a5560ec93ae59a590385918a240b92ef0763b
mehrah83 <92850875+mehrah83@users.noreply.github.com>
2022-02-15 19:03:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Shop from "./Shop";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Shop />
+      </div>
+    </>
+  );
+};
+
+export default App;
